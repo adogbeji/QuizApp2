9e2df070a530e0bc58854db7353a5468c474fdba
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-12-26 19:16:43 +0000

    Add Column widget to Scaffold & add 3 buttons

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,28 @@ class MyApp extends StatelessWidget {
           const Text('Questionnare App!', style: TextStyle(fontSize: 24), textAlign: TextAlign.center,),
           ),
         ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(6),
+              child: 
+            Text('Choose from below:', style: TextStyle(fontSize: 18), textAlign: TextAlign.center,),
+            ),
+            ElevatedButton(
+              child: Text('Register'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Login'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Contact'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
