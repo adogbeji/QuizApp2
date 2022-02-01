a89e517685a66016a352b29e28c555744aa16ffd
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-02-01 20:08:03 +0000

    Create answer widget

---
+++ lib/answer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+              child: Text('Register'),
+              onPressed: null,
+            );
+  }
+}
